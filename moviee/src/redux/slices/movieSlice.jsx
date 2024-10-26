4b08f1c43f6180e4f11cadfeceab34129c99b3ff
samet1qwer <samet102a@gmail.com>
2024-10-26 18:40:09 +0300

    Update movieSlice.jsx

--- moviee/src/redux/slices/movieSlice.jsx
+++ moviee/src/redux/slices/movieSlice.jsx
@@ -7,7 +7,7 @@ const initialState = {
 };
 
 export const getData = createAsyncThunk("getData", async (movieName) => {
-  const apiKey = "b5a0a1c4";
+  const apiKey = "<your api_key>";
   const url = `http://www.omdbapi.com/?s=${movieName}&apikey=${apiKey}`;
   const response = await axios.get(url);
   return response.data.Search;
